e369a05df517d1cd99210c1ba793ffd207ce3e35
Aryaman Patra <aryamanprivate26@gmail.com>
2023-09-06 08:56:59 +0530

    Added card views to HomeFragment

--- app/src/main/java/com/example/mentalpeaceapp/ui/home/HomeViewModel.java
+++ app/src/main/java/com/example/mentalpeaceapp/ui/home/HomeViewModel.java
@@ -10,7 +10,7 @@ public class HomeViewModel extends ViewModel {
 
     public HomeViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("This is home fragment");
+        mText.setValue("What service would you like to use?");
     }
 
     public LiveData<String> getText() {
